fbda8bc5720099c312c28f938b1ad5c0475589b9
Abhi2417 <ar972033@student.nitw.ac.in>
2026-08-31 15:04:27 +0530

    git commit -m "Add Dockerfile for Render deployment (Java 21)"

---
+++ src/test/java/com/app/citysparsh/CitySparshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.citysparsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitySparshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
